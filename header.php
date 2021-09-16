50b008ce1a29b4e58eb0db004187798b2a393f4b
Bernard Antwi <bernardantwi2010@gmail.com>
2021-09-16 09:55:09 +0000

    removed sidebar toggle

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
                     <ul class="navbar-nav mr-auto mt-md-0">
                         
                         <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted  " href="javascript:void(0)"><i class="mdi mdi-menu"></i></a> </li>
-                        <li class="nav-item m-l-10"> <a class="nav-link sidebartoggler hidden-sm-down text-muted  " href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
+                        <!-- <li class="nav-item m-l-10"> <a class="nav-link sidebartoggler hidden-sm-down text-muted  " href="javascript:void(0)"><i class="ti-menu"></i></a> </li> -->
                         
                     </ul>
                     
