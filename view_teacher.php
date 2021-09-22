e0ab8936dfd888a9b6269c17c52a23b9ae57393a
Bernard Antwi <bernardantwi2010@gmail.com>
2021-09-22 15:19:48 +0000

    add teacher updated

--- view_teacher.php
+++ view_teacher.php
@@ -25,7 +25,7 @@ if(isset($_GET['id']))
             
             <div class="row page-titles">
                 <div class="col-md-5 align-self-center">
-                    <h3 class="text-primary"> View Teacher</h3> </div>
+                    <h3 class="text-primary"> View Teacher </h3> </div>
                 <div class="col-md-7 align-self-center">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
