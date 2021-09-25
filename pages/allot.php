13d4aa94e5d65b1b5428fee5dd3bc54f017a4c5e
Bernard Antwi <bernardantwi2010@gmail.com>
2021-09-25 10:49:46 +0000

    update

--- pages/allot.php
+++ pages/allot.php
@@ -8,70 +8,29 @@ $success = false;
 extract($_POST);
 
 if (isset($btn_save)) {
-    $init = "SELECT SUM(alot_number) AS total from allot WHERE room_type_id = '$room_type_id' AND quiz_date = '$quiz_date'  AND start_time = '$start_time' AND end_time = '$end_time'";
-    // $check_alot = $conn->query("SELECT SUM(alot_number) AS total FROM allot WHERE room_type_id = '$room_type_id' AND quiz_date = '$quiz_date' AND start_time = '$start_time' ") or die(mysqli_error($conn));
+    $check_alot = $conn->query("SELECT SUM(alot_number) AS total FROM allot WHERE room_type_id = '$room_type_id' AND quiz_date = '$quiz_date' AND start_time = '$start_time' ") or die(mysqli_error($conn));
 
-    //check times
-
-//     "SELECT * FROM logs WHERE date BETWEEN '" . $from_date . "' AND  '" . $to_date . "'
-// ORDER by id DESC"
-// new update
-// new
-// new
-
-    // $check_time = $init." AND start_time BETWEEN '$start_time' AND  '$end_time' AND end_time BETWEEN '$start_time' AND  '$end_time'";
-
-    $check_time = $init . " AND start_time = '$start_time' AND end_time = '$end_time'";
-    
-    $query_string = $conn->query($init);
-
-    $check_alot_item = mysqli_fetch_assoc($query_string);
+    $check_alot_item = mysqli_fetch_assoc($check_alot);
 
     $current_alot_value = $check_alot_item['total'];
-    die(var_dump($check_alot_item));
+
     $seats_left = $room_capacity - $current_alot_value;
-    
-       if ($seats_left > $class_capacity) {
-           $success = false;
 
-           $errorMsg = $seats_left;
+    if ($seats_left > $class_capacity AND $start_time == $start_time) {
 
-           $error_html = '<p class="alert alert-success"><span class="fa fa-exclamation-circle"></span> '.$errorMsg.'</p>';
+        $add_allotment = $conn->query("INSERT INTO `allot` (`class_id`,`room_type_id`,`subject_id`,`level`,`quiz_date`,`start_time`,`end_time`, `alot_number`) VALUES ('$class_id','$room_type_id','$subject_id','$level','$quiz_date','$start_time','$end_time', '$class_capacity')");
 
-       }else{
-        $success = false;
+        $success = true;
 
-        $errorMsg = $seats_left;
+        $errorMsg = 'Booking successful';
 
         $error_html = '<p class="alert alert-success"><span class="fa fa-exclamation-circle"></span> '.$errorMsg.'</p>';
+    }
+    else{
+        $errorMsg = 'There would not be enough seats available on the set period';
 
-       }
-//    $check_alot_item = mysqli_fetch_assoc($check_alot);
-
-//    $current_alot_value = $check_alot_item['total'];
-
-//    $seats_left = $room_capacity - $current_alot_value;
-
-//    if ($seats_left > $class_capacity) {
-
-//        $add_allotment = $conn->query("INSERT INTO `allot` (`class_id`,`room_type_id`,`subject_id`,`level`,`quiz_date`,`start_time`,`end_time`, `alot_number`) VALUES ('$class_id','$room_type_id','$subject_id','$level','$quiz_date','$start_time','$end_time', '$class_capacity')");
-
-
-
-//        // $conn->query("INSERT INTO 'allot_student' ('allot_id', 'exam_id', 'exam_date', 'start_time', 'end_time', 'room_id', 'student_id', 'stud_id') VALUES ('$last_id', '$exam_id', '$quiz_date', '$start_time', '$end_time', '$room_type_id', '')");
-
-//        $success = true;
-
-//        $errorMsg = 'Booking successful';
-
-//        $error_html = '<p class="alert alert-success"><span class="fa fa-exclamation-circle"></span> '.$errorMsg.'</p>';
-
-
-//    }else{
-//         $errorMsg = 'There would not be enough seats available on the set period';
-
-//         $error_html = '<p class="alert alert-danger"><span class="fa fa-exclamation-circle"></span> '.$errorMsg.'</p>';
-//    }
+        $error_html = '<p class="alert alert-danger"><span class="fa fa-exclamation-circle"></span> '.$errorMsg.'</p>';
+    }
    
    
 }
@@ -79,7 +38,7 @@ if (isset($btn_save)) {
 
 ?>
 <script type="text/javascript">
-    let success = '<?= $success; ?>';
+    let success = '<?php $success; ?>';
     if(success == true){
         $('.errorMsg').toggleClass('alert alert-success');
     }
